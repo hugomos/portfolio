9724d8e6a88f727905248525753aa02cbed7ece5
Vitor Hugo Oliveira <vitor_osantos@hotmail.com>
2023-09-25 15:10:25 -0300

    style: alterando tamanho das fontes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Home from '@/pages/home.mdx'
 
 export default function Page() {
   return (
-    <article className="w-full p-8 py-12 prose prose-lg md:prose-xl prose-sky dark:prose-invert">
+    <article className="w-full p-8 py-12 prose prose-base md:prose-lg prose-sky dark:prose-invert">
       <Home />
     </article>
   )
