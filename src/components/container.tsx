6977dfefd18d9ae43ff803a32d9f20d4ace94483
Vitor Hugo Oliveira <vitor_osantos@hotmail.com>
2024-01-31 16:45:26 -0300

    style: correcting Container component styling

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -6,7 +6,7 @@ interface ContainerProps {
 
 export const Container: React.FC<ContainerProps> = ({ children }) => {
   return (
-    <div className="max-w-2xl mb-40 flex flex-col md:flex-row mx-4 mt-8 lg:mx-auto">
+    <div className="max-w-2xl mb-40 flex flex-col mx-4 mt-8 lg:mx-auto">
       {children}
     </div>
   )
