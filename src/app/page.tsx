0945ab99981cd8afea375f0fd64f43f89342972d
Vitor Hugo Oliveira <vitor_osantos@hotmail.com>
2024-02-01 07:05:58 -0300

    refactor: removing files from the first version

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Home from '@/pages/home.mdx'
-
-export default function Page() {
-  return (
-    <article className="w-full p-8 py-12 prose prose-base md:prose-lg prose-sky dark:prose-invert">
-      <Home />
-    </article>
-  )
-}
