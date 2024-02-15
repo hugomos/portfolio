23d9baf647e7858adf363d931da944298c2cd9f9
Vitor Hugo Oliveira <vitor_osantos@hotmail.com>
2024-02-15 15:54:27 -0300

    feat: adding contact links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import { Link } from '@/components/link'
 import { Section } from '@/components/section'
+import { ArrowTopRightIcon } from '@radix-ui/react-icons'
 import React from 'react'
 
 const HomePage: React.FC = () => {
   return (
     <Section.Root>
       <Section.Header>
-        <Section.Title>Hi, I'm hugomos 👋</Section.Title>
+        <Section.Title>hey, I'm hugomos 👋</Section.Title>
         <Section.Resume>
           I am a software developer, technology enthusiast, and very practical.
           Currently, I work in the development team at{' '}
@@ -36,6 +37,37 @@ const HomePage: React.FC = () => {
           in the development of these solutions.
         </Section.Resume>
       </Section.Header>
+      <Section.Content>
+        <Section.Divider />
+        <div className="flex items-center gap-2 w-full">
+          <Link
+            href="https://github.com/hugomos/"
+            variant="icon"
+            className="hover:underline"
+          >
+            <ArrowTopRightIcon />
+            GitHub
+          </Link>
+
+          <Link
+            href="https://linkedin.com/in/hugomos/"
+            variant="icon"
+            className="hover:underline"
+          >
+            <ArrowTopRightIcon />
+            Linkedin
+          </Link>
+
+          <Link
+            href="mailto:vitor_osantos@hotmail.com"
+            variant="icon"
+            className="hover:underline"
+          >
+            <ArrowTopRightIcon />
+            Email
+          </Link>
+        </div>
+      </Section.Content>
     </Section.Root>
   )
 }
