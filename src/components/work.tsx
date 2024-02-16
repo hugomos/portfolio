b4e7b180c62de249d4655d30f92e982cbab5b0a4
Vitor Hugo Oliveira <vitor_osantos@hotmail.com>
2024-02-16 07:18:21 -0300

    feat: dynamically searching github projects

--- src/components/work.tsx
+++ src/components/work.tsx
@@ -1,5 +1,5 @@
-import Link from 'next/link'
 import React from 'react'
+import { Link } from './link'
 import { Badge } from './ui/badge'
 
 export interface IWork {
@@ -8,6 +8,7 @@ export interface IWork {
   image: string
   description: string
   topics: string[]
+  html_url: string
 }
 
 interface WorkProps {
@@ -17,15 +18,15 @@ interface WorkProps {
 export const Work: React.FC<WorkProps> = ({ work }) => {
   return (
     <div className="relative group overflow-hidden rounded-md md:w-80">
-      <Link href="#">
+      <Link href={work.html_url} variant="none">
         <div className="absolute inset-0 bg-black/80 opacity-0 transition-opacity group-hover:opacity-100"></div>
         <img
-          src={work.image}
+          src={work.image ? work.image : '/default-image.png'}
           alt={work.name}
           className="w-full h-52 object-cover transition-opacity group-hover:opacity-25 md:w-80"
         />
         <div className="absolute inset-0 flex flex-col items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity">
-          <div className="p-8 text-white">
+          <div className="p-8 text-white max-w-full">
             <h2 className="text-xl font-bold mb-2">{work.name}</h2>
             <p className="text-xs mb-4 w-3/4 md:w-full">{work.description}</p>
             <div className="flex justify-start gap-2">
