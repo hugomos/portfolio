9f5ac079efa2363822f8681b08053dfce67627a7
Vitor Hugo Oliveira <vitor_osantos@hotmail.com>
2024-02-22 14:37:31 -0300

    style: restyling work component

--- src/components/work.tsx
+++ src/components/work.tsx
@@ -1,34 +1,33 @@
 import { Repository } from '@/app/api/repositories/route'
 import Link from 'next/link'
 import React from 'react'
-import { Badge } from './ui/badge'
 
 interface WorkProps {
   work: Repository
 }
 
 export const Work: React.FC<WorkProps> = ({ work }) => {
+  const name = work.name
+    .replace(/-/g, ' ')
+    .replace(/\b\w/g, (c) => c.toUpperCase())
+
   return (
     <Link
       href={work.html_url}
       target="_blank"
-      className="group w-full block p-4 space-y-4 bg-accent rounded-sm hover:bg-accent-foreground  transition-colors"
+      className="block w-full space-y-5 p-4 rounded-md transition-colors bg-secondary hover:bg-secondary-foreground/10"
     >
-      <div className="w-full flex justify-between items-center">
-        <h3 className="font-bold text-lg text-primary group-hover:text-secondary">
-          {work.name}
-        </h3>
+      <div className="w-full flex items-center justify-between">
+        <h3 className="font-bold">{name}</h3>
         <div className="flex items-center gap-2">
-          {work.topics
-            .filter((topic) => topic !== 'portfolio')
-            .map((topic) => (
-              <Badge key={topic} variant="default">
-                {topic}
-              </Badge>
-            ))}
+          {work.topics.map((topic) => (
+            <div className="text-xs font-semibold rounded-sm px-1.5 py-0.5 bg-secondary-foreground/10 dark:bg-secondary-foreground/20 ">
+              {topic}
+            </div>
+          ))}
         </div>
       </div>
-      <div className="text-muted-foreground text-sm">{work.description}</div>
+      <p className="text-sm text-muted-foreground">{work.description}</p>
     </Link>
   )
 }
