c197c04d132a115fc78fb002b1074d8f4e3a1fe7
Vitor Hugo Oliveira <vitor_osantos@hotmail.com>
2024-02-22 16:51:16 -0300

    refactor: removing string manipulations and replacing them with utility functions

--- src/components/work.tsx
+++ src/components/work.tsx
@@ -1,24 +1,21 @@
 import { Repository } from '@/app/api/repositories/route'
-import Link from 'next/link'
 import React from 'react'
+
+import { capitalize } from '@/utils/string/capitalize'
+import Link from 'next/link'
 
 interface WorkProps {
   work: Repository
 }
 
 export const Work: React.FC<WorkProps> = ({ work }) => {
-  const name = work.name
-    .replace(/-/g, ' ')
-    .replace(/\b\w/g, (c) => c.toUpperCase())
-
   return (
     <Link
-      href={work.html_url}
-      target="_blank"
+      href={`/work/${work.name}`}
       className="block w-full space-y-5 p-4 rounded-md transition-colors bg-secondary hover:bg-secondary-foreground/10"
     >
       <div className="w-full flex items-center justify-between">
-        <h3 className="font-bold">{name}</h3>
+        <h3 className="font-bold">{capitalize(work.name)}</h3>
         <div className="flex items-center gap-2">
           {work.topics.map((topic) => (
             <div className="text-xs font-semibold rounded-sm px-1.5 py-0.5 bg-secondary-foreground/10 dark:bg-secondary-foreground/20 ">
