3bba989e02024872762e795024875faf4831005e
Vitor Hugo Oliveira <vitor_osantos@hotmail.com>
2024-05-28 09:05:04 -0300

    refactor: rewriting texts on the professional experience page

--- src/app/experience/page.tsx
+++ src/app/experience/page.tsx
@@ -1,6 +1,5 @@
 import { Experience } from '@/components/experience'
 import { Section } from '@/components/section'
-import Link from 'next/link'
 import React from 'react'
 
 const ExperiencePage: React.FC = () => {
@@ -17,41 +16,57 @@ const ExperiencePage: React.FC = () => {
               Athenas Consultoria Agrícola e Laboratórios
             </Experience.Title>
             <Experience.Subtitle>
-              Software Developer, 2021 - Present
+              Software Developer, 2022 - Present
             </Experience.Subtitle>
             <Experience.Resume>
-              I joined Athenas early as part of the{' '}
-              <Link href="https://www.sp.senac.br/jovem-aprendiz-empresa">
-                Young Apprentice program at Senac São Paulo
-              </Link>
-              . I worked for about a year on the agricultural consulting team
-              and later transferred to the development team. I created scripts,
-              developed plugins, and tools for automating processes from the
-              office to the field.
+              I significantly contributed to the development of resources and
+              important improvements for the company's internal processes,
+              positively impacting dozens of clients. My achievements include:
             </Experience.Resume>
             <Experience.Highlights.Root>
               <Experience.Highlights.Item>
-                In 2021, within the consulting team, I gained insights into the
-                significance of georeferenced points in precision agriculture.
-                Using GPS, we marked points to collect data, such as soil
-                samples. This data guides decisions regarding inputs and
-                irrigation. Sending samples to the laboratory, based on these
-                points, generates personalized recommendations, optimizing
-                agricultural production and promoting sustainability.
+                Designing and developing a plugin for QGIS, allowing data
+                visualization in graphs, creating sample collection tasks, and
+                visualizing and generating routes for sending to the collection
+                app.
               </Experience.Highlights.Item>
               <Experience.Highlights.Item>
-                In 2022, already integrated into the development team, I
-                contributed to the development of automation scripts using the
-                Python language. I wrote technical documentation on the
-                processes and scripts, and I developed a website with Next.js
-                and TypeScript to make the documentation available.
+                Collaborating on the planning and delivery of the current system
+                used in the Consulting department.
               </Experience.Highlights.Item>
               <Experience.Highlights.Item>
-                In 2023, I actively participated in decision-making related to
-                the software developed by the team. I created tools and improved
-                existing scripts. I also took part in the process of rewriting
-                legacy code and in the development of a unified process
-                management system.
+                Developing and implementing functionalities for automatic update
+                and upload of georeferenced geometries in the sample collection
+                app.
+              </Experience.Highlights.Item>
+            </Experience.Highlights.Root>
+          </Experience.Root>
+
+          <Experience.Root>
+            <Experience.Title>
+              Athenas Consultoria Agrícola e Laboratórios
+            </Experience.Title>
+            <Experience.Subtitle>
+              Software Developer, 2021 - 2022
+            </Experience.Subtitle>
+            <Experience.Resume>
+              I had the opportunity to learn about the processes and challenges
+              of the Consulting and IT departments, as well as deepen my
+              knowledge in precision agriculture and soil chemical analysis. My
+              achievements during this period include:
+            </Experience.Resume>
+            <Experience.Highlights.Root>
+              <Experience.Highlights.Item>
+                Developing a hub using Django, which centralizes technical
+                documentation of the company's internal processes and scripts.
+              </Experience.Highlights.Item>
+              <Experience.Highlights.Item>
+                Deepening my understanding of the Python language, technical
+                documentation development, architectures, and design patterns.
+              </Experience.Highlights.Item>
+              <Experience.Highlights.Item>
+                Working closely with the consulting and IT teams to improve
+                process efficiency and service quality.
               </Experience.Highlights.Item>
             </Experience.Highlights.Root>
           </Experience.Root>
