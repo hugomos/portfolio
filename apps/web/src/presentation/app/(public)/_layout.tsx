7c2c16ed168fc834f68789728d87551a77f957ff
Vitor Hugo Oliveira <vitor_osantos@hotmail.com>
2026-06-22 17:00:58 -0300

    feat(web): adicionar navigation menu componentizado e corrigir padding responsivo do layout

--- apps/web/src/presentation/app/(public)/_layout.tsx
+++ apps/web/src/presentation/app/(public)/_layout.tsx
@@ -1,10 +1,14 @@
 import type React from "react";
 import { Outlet } from "react-router";
+import { Header } from "./@components/header";
 
 export const PublicRootLayout: React.FC = () => {
 	return (
-		<main className="mx-auto max-w-2xl px-8 py-10 md:px-10">
-			<Outlet />
-		</main>
+		<div className="mx-auto max-w-2xl space-y-8 px-4 py-8 sm:px-6 sm:py-10 md:px-8">
+			<Header />
+			<main>
+				<Outlet />
+			</main>
+		</div>
 	);
 };
