38b3e1f2f46f189f2757eeec248fb57a848bf6bb
Vitor Hugo Oliveira <vitor_osantos@hotmail.com>
2026-06-22 16:17:58 -0300

    feat(web): adicionar theme provider e integrar ao layout

--- apps/web/src/presentation/app/index.tsx
+++ apps/web/src/presentation/app/index.tsx
@@ -1,13 +1,16 @@
 import type React from "react";
 import { BrowserRouter, Route, Routes } from "react-router";
+import { ThemeProvider } from "../components/theme-provider";
 import { Home } from "./home";
 
 export const App: React.FC = () => {
 	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<Home />} />
-			</Routes>
-		</BrowserRouter>
+		<ThemeProvider defaultTheme="dark" storageKey="hugomos-ui-theme">
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<Home />} />
+				</Routes>
+			</BrowserRouter>
+		</ThemeProvider>
 	);
 };
