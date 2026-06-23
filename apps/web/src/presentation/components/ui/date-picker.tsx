07eecb398935a9e06fe64c531dc25b6fab6d22e0
Vitor Hugo Oliveira <vitor_osantos@hotmail.com>
2026-06-23 14:04:22 -0300

    feat(web): adicionar componentes shadcn

---
+++ apps/web/src/presentation/components/ui/date-picker.tsx
@@ -0,0 +1,40 @@
+"use client"
+
+import * as React from "react"
+import { format } from "date-fns"
+import { ChevronDownIcon } from "lucide-react"
+
+import { Button } from "@/presentation/components/ui/button"
+import { Calendar } from "@/presentation/components/ui/calendar"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/presentation/components/ui/popover"
+
+export function DatePicker() {
+  const [date, setDate] = React.useState<Date>()
+
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button
+          variant="outline"
+          data-empty={!date}
+          className="w-[212px] justify-between text-left font-normal data-[empty=true]:text-muted-foreground"
+        >
+          {date ? format(date, "PPP") : <span>Pick a date</span>}
+          <ChevronDownIcon />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-auto p-0" align="start">
+        <Calendar
+          mode="single"
+          selected={date}
+          onSelect={setDate}
+          defaultMonth={date}
+        />
+      </PopoverContent>
+    </Popover>
+  )
+}
