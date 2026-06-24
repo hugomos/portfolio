63005f30f2a08ac58e05a5e672bc351cc5e0e574
Vitor Hugo Oliveira <vitor_osantos@hotmail.com>
2026-06-24 13:36:40 -0300

    feat(web): adicionar filtro por visibilidade nos hooks de projetos e experiências

--- apps/web/src/modules/experience/hooks/use-experiences.tsx
+++ apps/web/src/modules/experience/hooks/use-experiences.tsx
@@ -1,14 +1,23 @@
 import type { ExperienceDTO } from "../dto";
 import { mock } from "../mock";
 
+interface UseExperiencesProps {
+	visible?: boolean;
+}
+
 interface UseExperiences {
 	experiences: ExperienceDTO[] | undefined;
 	experiencesIsLoading: boolean;
 }
 
-export function useExperiences(): UseExperiences {
+export function useExperiences({
+	visible,
+}: UseExperiencesProps): UseExperiences {
 	return {
-		experiences: mock,
+		experiences: mock.filter((exp) => {
+			if (visible === undefined) return true;
+			return exp.visible === visible;
+		}),
 		experiencesIsLoading: false,
 	};
 }
--- apps/web/src/modules/project/hooks/use-projects.tsx
+++ apps/web/src/modules/project/hooks/use-projects.tsx
@@ -1,14 +1,21 @@
 import type { ProjectDTO } from "../dto";
 import { mock } from "../mock";
 
+interface UseProjectsProps {
+	visible?: boolean;
+}
+
 interface UseProjects {
 	projects: ProjectDTO[] | undefined;
 	projectsIsLoading: boolean;
 }
 
-export function useProjects(): UseProjects {
+export function useProjects({ visible }: UseProjectsProps): UseProjects {
 	return {
-		projects: mock,
+		projects: mock.filter((p) => {
+			if (visible === undefined) return true;
+			return p.visible === visible;
+		}),
 		projectsIsLoading: false,
 	};
 }
