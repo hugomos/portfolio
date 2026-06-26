d59695bdcf5d7e14c639a8d085ca8f52046a1400
Vitor Hugo Oliveira <vitor_osantos@hotmail.com>
2026-06-26 09:54:48 -0300

    fix(server): ignorar autenticação na rota de health check [vercel skip]

--- apps/server/src/modules/identity/features/authentication/application/middlewares/token.ts
+++ apps/server/src/modules/identity/features/authentication/application/middlewares/token.ts
@@ -5,6 +5,7 @@ export const tokenMiddleware = fp(async (app: FastifyInstance) => {
 	app.addHook("preHandler", async (request) => {
 		if (request.routeOptions.config.public) return;
 		if (request.url.startsWith("/api/docs")) return;
+		if (request.url === "/health") return;
 
 		const { sub: id } = await request.jwtVerify<{
 			sub: string;
